821552e910d4e33b397e3cd6019154ebd3f96563
Andrew Raub <raublekick@gmail.com>
2021-01-24 20:14:12 -0700

    Fixing nav bar and improving mobile layout

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,18 +2,12 @@
   <b-navbar>
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
-        />
+        <b-icon icon="mushroom" size="is-large" />
       </b-navbar-item>
     </template>
     <template #start>
-      <b-navbar-item href="#">
+      <!-- <b-navbar-item href="#">
         Home
-      </b-navbar-item>
-      <b-navbar-item href="#">
-        Documentation
       </b-navbar-item>
       <b-navbar-dropdown label="Info">
         <b-navbar-item href="#">
@@ -22,20 +16,15 @@
         <b-navbar-item href="#">
           Contact
         </b-navbar-item>
-      </b-navbar-dropdown>
-    </template>
-
-    <template #end>
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
+      </b-navbar-dropdown> -->
+      <b-navbar-item
+        tag="a"
+        href="https://github.com/raublekick/sm64-scavenger-hunt"
+      >
+        <b-icon icon="github" />
       </b-navbar-item>
     </template>
+
+    <template #end> </template>
   </b-navbar>
 </template>
--- src/components/Randomizer.vue
+++ src/components/Randomizer.vue
@@ -1,58 +1,69 @@
 <template>
   <div>
-    <b-field grouped>
-      <b-field label="Difficulty">
-        <b-select v-model="options.difficulty">
-          <option value="1">Easy</option>
-          <option value="2">Normal</option>
-          <option value="3">Hard</option>
-        </b-select>
-      </b-field>
-      <b-field label="Min number of rules">
-        <b-input type="number" v-model="options.minNumberOfRules"></b-input>
-      </b-field>
-      <b-field label="Max number of rules">
-        <b-input type="number" v-model="options.maxNumberOfRules"></b-input>
-      </b-field>
-    </b-field>
-    <p class="control">
-      <b-button class="is-primary" @click="randomizeSelectedRules(options)"
-        >Randomize</b-button
-      >
-    </p>
-    <div v-if="selectedRules.length">
-      <b-table
-        :data="selectedRules"
-        detailed
-        detail-key="title"
-        :show-detail-icon="true"
-      >
-        <b-table-column field="title" label="Name" v-slot="props" searchable>
-          {{ props.row.title }}
-        </b-table-column>
-        <b-table-column field="tags" label="Tags" v-slot="props" searchable>
-          <span
-            v-for="(tag, i) in !props.row.tags ? [] : props.row.tags.split(',')"
-            :key="i"
-            class="tag is-success mr-3"
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <b-field label="Difficulty">
+          <b-select v-model="options.difficulty">
+            <option value="1">Easy</option>
+            <option value="2">Normal</option>
+            <option value="3">Hard</option>
+          </b-select>
+        </b-field>
+        <b-field label="Min number of rules">
+          <b-input type="number" v-model="options.minNumberOfRules"></b-input>
+        </b-field>
+        <b-field label="Max number of rules">
+          <b-input type="number" v-model="options.maxNumberOfRules"></b-input>
+        </b-field>
+        <p class="control">
+          <b-button class="is-primary" @click="randomizeSelectedRules(options)"
+            >Randomize</b-button
           >
-            {{ tag }}
-          </span>
-        </b-table-column>
+        </p>
+      </div>
+      <div class="column">
+        <div v-if="selectedRules.length">
+          <b-table
+            :data="selectedRules"
+            detailed
+            detail-key="title"
+            :show-detail-icon="true"
+          >
+            <b-table-column
+              field="title"
+              label="Name"
+              v-slot="props"
+              searchable
+            >
+              {{ props.row.title }}
+            </b-table-column>
+            <b-table-column field="tags" label="Tags" v-slot="props" searchable>
+              <span
+                v-for="(tag, i) in !props.row.tags
+                  ? []
+                  : props.row.tags.split(',')"
+                :key="i"
+                class="tag is-success mr-3"
+              >
+                {{ tag }}
+              </span>
+            </b-table-column>
 
-        <b-table-column
-          field="difficulty"
-          label="Difficulty"
-          v-slot="props"
-          searchable
-        >
-          {{ props.row.difficulty }}
-        </b-table-column>
+            <b-table-column
+              field="difficulty"
+              label="Difficulty"
+              v-slot="props"
+              searchable
+            >
+              {{ props.row.difficulty }}
+            </b-table-column>
 
-        <template #detail="props">
-          <div>{{ props.row.notes }}</div>
-        </template>
-      </b-table>
+            <template #detail="props">
+              <div>{{ props.row.notes }}</div>
+            </template>
+          </b-table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
